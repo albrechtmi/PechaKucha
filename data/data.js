efe06bcda78167c46a13d3dda2c21937dc1d858d
Michael Albrecht <albrechtmichael@live.com>
2018-02-06 10:14:16 +0100

    Pecha Kucha Player
    Initial commit for Pecha Kucha at University of Zürich

---
+++ data/data.js
@@ -0,0 +1,404 @@
+var sData = '{'+
+  '"slides": [' +
+'    {'+
+'      "slideNo": 1,'+
+'      "title": "Pecha Kucha",'+
+'      "textHTML": "<p><h3>Albrecht Michael</H3></p>",'+
+'      "imgWidth": "300px",'+
+'      "imgHeight": "300px",'+
+'      "imageSrc": "images/albrechtMichael.png",'+
+'      "imageMarginTop": "0px",'+
+'      "progressBarColor": "#0028A5",'+
+'      "progressBarColorBackground": "#DADEE2",'+
+'      "titleColor": "#333333",'+
+'      "textColor": "#666666",'+
+'      "borderColor": "#0028A5"'+
+'    }'+
+
+'    ,{'+
+'      "slideNo": 2,'+
+'      "title": "Allgemein",'+
+'      "textHTML": "<p><h3>Geboren 8. M&auml;rz 1985 in Z&uumlrich (Triemli)</h3>'+
+'      <h3>Aufgewachsen in Getwing (Wallis)</h3>'+
+'      <h3>Ledig</h3><h3>Mitbewohner Lancelot & Timmy<h3></p>",'+
+'      "imgWidth": "800px",'+
+'      "imgHeight": "300px",'+
+'      "imageSrc": "images/getwing.png",'+
+'      "imageMarginTop": "0px",'+
+'      "progressBarColor": "#0028A5",'+
+'      "progressBarColorBackground": "#DADEE2",'+
+'      "titleColor": "#333333",'+
+'      "textColor": "#666666",'+
+'      "borderColor": "#0028A5"'+
+'    }'+
+
+'    ,{'+
+'      "slideNo": 3,'+
+'      "title": "Allgemein",'+
+'      "textHTML": "<p><h3>2 Schwestern</h3><h3>D&uuml;dingen</h3><h3>Worblaufen</h3>",'+
+'      "imgWidth": "200px",'+
+'      "imgHeight": "300px",'+
+'      "imageSrc": "images/geschwister.png",'+
+'      "imageMarginTop": "0px",'+
+'      "progressBarColor": "#0028A5",'+
+'      "progressBarColorBackground": "#DADEE2",'+
+'      "titleColor": "#333333",'+
+'      "textColor": "#666666",'+
+'      "borderColor": "#0028A5"'+
+'    }'+
+
+'    ,{' +
+'      "slideNo": 4,'+
+'      "title": "Allgemein",'+
+'      "textHTML": "<p><H3>Getti seit 2011</H3><H3>Familienmensch</H3></p>",'+
+'      "imgWidth": "400px",'+
+'      "imgHeight": "300px",'+
+'      "imageSrc": "images/maximilian.jpg",'+
+'      "imageMarginTop": "0px",'+
+'      "progressBarColor": "#0028A5",'+
+'      "progressBarColorBackground": "#DADEE2",'+
+'      "titleColor": "#333333",'+
+'      "textColor": "#666666",'+
+'      "borderColor": "#0028A5"'+
+'    }' +
+
+'    ,{'+
+'      "slideNo": 5,'+
+'      "title": "Werdegang Studium",'+
+'      "textHTML": "<p><H3>Wirtschaftsgymnasium Brig<H3><H3>Studium Betriebs&ouml;konomie</h3><h3>Vertiefungsrichtung HR</H3><h3>SAP Modulberater (zert.)</h3></p>",'+
+'      "imgWidth": "500px",'+
+'      "imgHeight": "300px",'+
+'      "imageSrc": "images/spiritus.jpg",'+
+'      "imageMarginTop": "0px",'+
+'      "progressBarColor": "#077714",'+
+'      "progressBarColorBackground": "#9fe0a5",'+
+'      "titleColor": "#0fba03",'+
+'      "textColor": "#0a7f02",'+
+'      "borderColor": "#0fba03"'+
+'    }'+
+
+'    ,{'+
+'      "slideNo": 6,'+
+'      "title": "Anstellungen Studium",'+
+'      "textHTML": "<p><H3>Diverse Praktika & Sommerjobs</h3><h3>Baugewerbe</h3><h3>Versicherung</h3><h3>Bank</h3><h3>Spital</h3><h3>Tourismus</h3><h3>Retail/Shop</h3></p>",'+
+'      "imgWidth": "400px",'+
+'      "imgHeight": "300px",'+
+'      "imageSrc": "images/hotelkuche.jpg",'+
+'      "imageMarginTop": "0px",'+
+'      "progressBarColor": "#077714",'+
+'      "progressBarColorBackground": "#9fe0a5",'+
+'      "titleColor": "#0fba03",'+
+'      "textColor": "#0a7f02",'+
+'      "borderColor": "#0fba03"'+
+'    }'+
+
+'    ,{'+
+'      "slideNo": 7,'+
+'      "title": "Werdegang Beruflich",'+
+'      "textHTML": "<p>'+
+'      <H3>Selbst&auml;ndige Web Entwicklung w&auml;hrend Studium</h3>'+
+'      <h3>Ticketreservationen</h3>'+
+'      <h3>Hotelreservationen</h3>'+
+'      <h3>Statistiken</h3>'+
+'      <h3>CMS & Publikationsprodukte</h3></p>",'+
+'      "imgWidth": "400px",'+
+'      "imgHeight": "300px",'+
+'      "imageSrc": "images/webentwicklung.jpg",'+
+'      "imageMarginTop": "0px",'+
+'      "progressBarColor": "#077714",'+
+'      "progressBarColorBackground": "#9fe0a5",'+
+'      "titleColor": "#0fba03",'+
+'      "textColor": "#0a7f02",'+
+'      "borderColor": "#0fba03"'+
+'    }'+
+
+'    ,{'+
+'      "slideNo": 8,'+
+'      "title": "Werdegang Milit&auml;r",'+
+'      "textHTML": "<p>'+
+'      <H3>FLAB Rekrutenschule Radarsoldat</h3>'+
+'      <H3>Unteroffiziersschule (Zert. Teamleader & weitere Module)<H3>'+
+'      <H3>Oberwachtmeister<H3>'+
+'      <H3>Botschaftenschutz Genf (300 Personen)</h3>'+
+'      <h3>Projektarbeiten im VBS<h3>'+
+'      </p>",'+
+'      "imgWidth": "300px",'+
+'      "imgHeight": "300px",'+
+'      "imageSrc": "images/flab.jpg",'+
+'      "imageMarginTop": "0px",'+
+'      "progressBarColor": "#077714",'+
+'      "progressBarColorBackground": "#9fe0a5",'+
+'      "titleColor": "#0fba03",'+
+'      "textColor": "#0a7f02",'+
+'      "borderColor": "#0fba03"'+
+'    }'+
+
+'    ,{'+
+'      "slideNo": 9,'+
+'      "title": "Werdegang Beruflich",'+
+'      "textHTML": "<p>'+
+'      <H3>solutionXS</h3>'+
+'      <h3>2010 ABAP Entwickler (Junior)</h3>'+
+'      <h3>Modulberatung & Entwicklung beim Kunden</h3>'+
+'      <h3>Unterst&uuml;tzung bei Produktentwicklung</h3>'+
+'      </p>",'+
+'      "imgWidth": "450px",'+
+'      "imgHeight": "60px",'+
+'      "imageSrc": "images/sxs-logo.png",'+
+'      "imageMarginTop": "100px",'+
+'      "progressBarColor": "#077714",'+
+'      "progressBarColorBackground": "#9fe0a5",'+
+'      "titleColor": "#0fba03",'+
+'      "textColor": "#0a7f02",'+
+'      "borderColor": "#0fba03"'+
+'    }'+
+
+'    ,{'+
+'      "slideNo": 10,'+
+'      "title": "Werdegang Beruflich",'+
+'      "textHTML": "<p>'+
+'      <H3>Post CH AG Informationstechnologie</h3>'+
+'      <h3>2013 ABAP Entwickler (Professional)</h3>'+
+'      <h3>Ab 2015 ABAP Developer (Senior)</h3>'+
+'      <h3>Ab 2015 Technische Projektleitung, Lead Entwickler Konzernprojekte</h3>'+
+'      <h3>Swiss ICT Nachwuchskampagne</h3>'+
+'      </p>",'+
+'      "imgWidth": "500px",'+
+'      "imgHeight": "300px",'+
+'      "imageSrc": "images/postImage.png",'+
+'      "imageMarginTop": "0px",'+
+'      "progressBarColor": "#FFCC00",'+
+'      "progressBarColorBackground": "#EBE4D1",'+
+'      "titleColor": "#FFCC00",'+
+'      "textColor": "#0028A5",'+
+'      "borderColor": "#FFCC00"'+
+'    }'+
+
+
+
+'    ,{'+
+'      "slideNo": 11,'+
+'      "title": "Werdegang Beruflich - Projekte / T&auml;tigkeiten bei der Post",'+
+'      "textHTML": "<p>'+
+'      <h3>Postauto Abrechnung Drittunternehmen (ABAP im ECC)</h3>'+
+'      <h3>Post Immobilien Spesenapplikation (Custom Fiori ~4 Entwickler)</h3>'+
+'      <h3>Post Immobilien Auftragsmanagement (Custom Fiori ~6 Entwickler)</h3>'+
+'      <h3>Post Konzern Rapportierungsl&ouml;sung</h3>'+
+'      <h3>Post Konzern ESR Standardisierung & Implementierung</h3>'+
+'      </p>",'+
+'      "imgWidth": "500px",'+
+'      "imgHeight": "300px",'+
+'      "imageSrc": "images/postauto.jpg",'+
+'      "imageMarginTop": "0px",'+
+'      "progressBarColor": "#FFCC00",'+
+'      "progressBarColorBackground": "#EBE4D1",'+
+'      "titleColor": "#FFCC00",'+
+'      "textColor": "#0028A5",'+
+'      "borderColor": "#FFCC00"'+
+'    }'+
+
+'    ,{'+
+'      "slideNo": 12,'+
+'      "title": "Werdegang Beruflich - Projekte / T&auml;tigkeiten bei der Post",'+
+'      "textHTML": "<p>'+
+'      <h3>Fiori Lead Entwicklung & Aufbau UI5 Kompetenz</h3>'+
+'      <h3>Unterst&uuml;ng & Konzeption SAP Architektur, Basis, Rollen- & Berechtigungen</h3>'+
+'      <h3>Schulungen, Referate & Weiterbildungen</h3>'+
+'      </p>",'+
+'      "imgWidth": "500px",'+
+'      "imgHeight": "300px",'+
+'      "imageSrc": "images/sitBern2017_slides_albrechtmi.bmp",'+
+'      "imageMarginTop": "0px",'+
+'      "progressBarColor": "#FFCC00",'+
+'      "progressBarColorBackground": "#EBE4D1",'+
+'      "titleColor": "#FFCC00",'+
+'      "textColor": "#0028A5",'+
+'      "borderColor": "#FFCC00"'+
+'    }'+
+
+'    ,{'+
+'      "slideNo": 13,'+
+'      "title": "Private Engagements im SAP Umfeld",'+
+'      "textHTML": "<p>'+
+'      <h3>SAP Insidetrack Bern (Organisation und Referate)</h3>'+
+'      <h3>SAP codeJams (Organisation und Betreuung)</h3>'+
+'      </p>",'+
+'      "imgWidth": "500px",'+
+'      "imgHeight": "300px",'+
+'      "imageSrc": "images/sitbern.jpg",'+
+'      "imageMarginTop": "0px",'+
+'      "progressBarColor": "#0028A5",'+
+'      "progressBarColorBackground": "#DADEE2",'+
+'      "titleColor": "#333333",'+
+'      "textColor": "#666666",'+
+'      "borderColor": "#0028A5"'+
+'    }'+
+
+'    ,{'+
+'      "slideNo": 14,'+
+'      "title": "Private Engagements im SAP Umfeld",'+
+'      "textHTML": "<p>'+
+'      <h3>SAP Stammtische (Organisation)</h3>'+
+'      </p>",'+
+'      "imgWidth": "350px",'+
+'      "imgHeight": "150px",'+
+'      "imageSrc": "images/stammtischbern.jpg",'+
+'      "imageMarginTop": "100px",'+
+'      "progressBarColor": "#0028A5",'+
+'      "progressBarColorBackground": "#DADEE2",'+
+'      "titleColor": "#333333",'+
+'      "textColor": "#666666",'+
+'      "borderColor": "#0028A5"'+
+'    }'+
+
+'    ,{'+
+'      "slideNo": 15,'+
+'      "title": "Programmiersprachen",'+
+'      "textHTML": "<p>'+
+'      <h3>ABAP / ABAP OO</h3>'+
+'      <h3>Java</h3>'+
+'      <h3>PHP</h3>'+
+'      <h3>HTML/CSS/JavaScript</h3>'+
+'      <h3>C/C#</h3>'+
+'      <h3>Ruby</h3>'+
+'      <h3>Python</h3>'+
+'      <h3>und weitere</h3>'+
+'      </p>",'+
+'      "imgWidth": "300px",'+
+'      "imgHeight": "300px",'+
+'      "imageSrc": "images/webentwicklung.jpg",'+
+'      "imageMarginTop": "0px",'+
+'      "progressBarColor": "#0028A5",'+
+'      "progressBarColorBackground": "#DADEE2",'+
+'      "titleColor": "#333333",'+
+'      "textColor": "#666666",'+
+'      "borderColor": "#0028A5"'+
+'    }'+
+
+'    ,{'+
+'      "slideNo": 16,'+
+'      "title": "Warum Uni Z&uuml;rich",'+
+'      "textHTML": "<p>'+
+'      <h3>Neue Branche</h3>'+
+'      <h3>Zur&uuml;ck zu den SAP Wurzeln (regional)</h3>'+
+'      <h3>Neue Herausforderungen</h3>'+
+'      </p>",'+
+'      "imgWidth": "300px",'+
+'      "imgHeight": "180px",'+
+'      "imageSrc": "images/unizh.png",'+
+'      "imageMarginTop": "100px",'+
+'      "progressBarColor": "#0028A5",'+
+'      "progressBarColorBackground": "#DADEE2",'+
+'      "titleColor": "#333333",'+
+'      "textColor": "#666666",'+
+'      "borderColor": "#0028A5"'+
+'    }'+
+
+'    ,{'+
+'      "slideNo": 17,'+
+'      "title": "Hobbies - offline",'+
+'      "textHTML": "<p>'+
+'      <h3>Schwimmen</h3>'+
+'      </p>",'+
+'      "imgWidth": "400px",'+
+'      "imgHeight": "300px",'+
+'      "imageSrc": "images/schwimmen.jpg",'+
+'      "imageMarginTop": "0px",'+
+'      "progressBarColor": "#efaa21",'+
+'      "progressBarColorBackground": "#efd592",'+
+'      "titleColor": "##efaa21",'+
+'      "textColor": "##efaa21",'+
+'      "borderColor": "#efd592"'+
+'    }'+
+
+'    ,{'+
+'      "slideNo": 18,'+
+'      "title": "Hobbies - Offline",'+
+'      "textHTML": "<p>'+
+'      <h3>Lesen</h3>'+
+'      <h3>2~3 B&uuml;cher pro Woche</h3>'+
+'      </p>",'+
+'      "imgWidth": "300px",'+
+'      "imgHeight": "300px",'+
+'      "imageSrc": "images/lesen.jpg",'+
+'      "imageMarginTop": "0px",'+
+'      "progressBarColor": "#efaa21",'+
+'      "progressBarColorBackground": "#efd592",'+
+'      "titleColor": "##efaa21",'+
+'      "textColor": "##efaa21",'+
+'      "borderColor": "#efd592"'+
+'    }'+
+
+'    ,{'+
+'      "slideNo": 19,'+
+'      "title": "Hobbies - offline",'+
+'      "textHTML": "<p>'+
+'      <h3>VW Golf R</h3>'+
+'      </p>",'+
+'      "imgWidth": "500px",'+
+'      "imgHeight": "300px",'+
+'      "imageSrc": "images/golfR.jpg",'+
+'      "imageMarginTop": "0px",'+
+'      "progressBarColor": "#efaa21",'+
+'      "progressBarColorBackground": "#efd592",'+
+'      "titleColor": "##efaa21",'+
+'      "textColor": "##efaa21",'+
+'      "borderColor": "#efd592"'+
+'    }'+
+
+'    ,{'+
+'      "slideNo": 20,'+
+'      "title": "Hobbies - Online",'+
+'      "textHTML": "<p>'+
+'      <h3>Diablo III - Top 1000 Weltweit</h3>'+
+'      </p>",'+
+'      "imgWidth": "200px",'+
+'      "imgHeight": "300px",'+
+'      "imageSrc": "images/diablo3.jpg",'+
+'      "imageMarginTop": "0px",'+
+'      "progressBarColor": "#efaa21",'+
+'      "progressBarColorBackground": "#efd592",'+
+'      "titleColor": "##efaa21",'+
+'      "textColor": "##efaa21",'+
+'      "borderColor": "#efd592"'+
+'    }'+
+
+'    ,{'+
+'      "slideNo": 21,'+
+'      "title": "Hobbies - Online",'+
+'      "textHTML": "<p>'+
+'      <h3>Android App Development</h3>'+
+'      <h3>Diverse Spiele</h3>'+
+'      <h3>Clients f&uuml;r Vereine</h3>'+
+'      <h3>Spiele Companions</h3>'+
+'      </p>",'+
+'      "imgWidth": "300px",'+
+'      "imgHeight": "300px",'+
+'      "imageSrc": "images/android.jpg",'+
+'      "imageMarginTop": "0px",'+
+'      "progressBarColor": "#efaa21",'+
+'      "progressBarColorBackground": "#efd592",'+
+'      "titleColor": "##efaa21",'+
+'      "textColor": "##efaa21",'+
+'      "borderColor": "#efd592"'+
+'    }'+
+
+'    ,{'+
+'      "slideNo": 22,'+
+'      "title": "",'+
+'      "textHTML": "<H3>Merci</H3><br>' +
+'      <H2>Twitter: https://twitter.com/albrechtmi?lang=de</h2>' +
+'      <H2>GitHub: https://github.com/albrechtmi</h2>' +
+'      ",'+
+'      "imgWidth": "300px",'+
+'      "imgHeight": "300px",'+
+'      "imageSrc": "images/albrechtMichael.png",'+
+'      "imageMarginTop": "0px",'+
+'      "progressBarColor": "#0028A5",'+
+'      "progressBarColorBackground": "#DADEE2",'+
+'      "titleColor": "#333333",'+
+'      "textColor": "#666666",'+
+'      "borderColor": "#0028A5"'+
+'    }'+
+'  ]'+
+'}';
---
+++ src/main.js
@@ -0,0 +1,123 @@
+var pechaKuchaData;
+var pechaKuchaSlideNo = 0;
+var intervalNumber;
+var seconds = 0;
+var timePerSlide = 6; //21
+var startSlide = 1;
+
+function loadPechaKucha() {
+  //initiales laden
+  pechaKuchaData = JSON.parse(sData);
+
+  //setting initial values
+  //debugger;
+  var slide = pechaKuchaData.slides[pechaKuchaSlideNo]; //Start Satz
+  setSlideData(slide);
+  //prepare for start
+  pechaKuchaSlideNo = 0;
+  setFolienCounter(pechaKuchaSlideNo);
+
+  if(config.withText == true) {
+    document.getElementById("pechaKuchaStartImgText").style.display = "";
+  } else {
+    document.getElementById("pechaKuchaStartImgText").style.display = "none";
+  }
+
+}
+
+function setFolienCounter(slideNumber) {
+  if(slideNumber == 0 ) {
+    return;
+  }
+
+  if(slideNumber == pechaKuchaData.slides.length - 1 ) {
+    return;
+  }
+
+  var slideNo = slideNumber;
+  var sSlidesAnz = pechaKuchaData.slides.length - 2;
+  document.getElementById("foliencounter").innerHTML = slideNo + "/" + sSlidesAnz;
+}
+
+function startPechaKucha() {
+  document.getElementById("pechaKuchaStartImg").style.display = "none";
+  document.getElementById("pechaKuchaStartImgText").style.display = "none";
+  intervalNumber = 0;
+  setProgressBar(0);
+  pechaKuchaSlideNo = startSlide;
+  var slide = pechaKuchaData.slides[pechaKuchaSlideNo];
+  setSlideData(slide);
+  add1SecondFunction();
+  setFolienCounter(pechaKuchaSlideNo);
+}
+
+function startPechaKuchaText() {
+  document.getElementById("pechaKuchaText").style.display = "block";
+  startPechaKucha();
+}
+
+function nextSlide() {
+  pechaKuchaSlideNo = pechaKuchaSlideNo + 1;
+  seconds = 0;
+  if(pechaKuchaSlideNo == pechaKuchaData.slides.length ) {
+    //Bei letzter Folie angekommen - STOP();
+    document.getElementById("pechaKuchaStartImgLayer").style.display = "";
+    var correctLength = timePerSlide - 1;
+    setProgressBar(correctLength);
+    document.getElementById("pechaKuchaStartImg").style.display = "";
+    document.getElementById("pechaKuchaStartImgText").style.display = "";
+    return;
+  }
+  var slide = pechaKuchaData.slides[pechaKuchaSlideNo];
+  setSlideData(slide); //Daten setzen
+  setFolienCounter(pechaKuchaSlideNo);
+  add1SecondFunction(); //Timer fortsetzen
+}
+
+function setSlideData(slide) {
+  document.getElementById("pechaKuchaTitle").innerHTML = slide.title;
+  document.getElementById("pechaKuchaText").innerHTML = slide.textHTML;
+
+  //Bildinfos setzen
+  var img = document.getElementById("pechaKuchaImageSrc");
+  img.style.height = slide.imgHeight;
+  img.style.width = slide.imgWidth;
+  img.style.marginTop = slide.imageMarginTop;
+  img.src = slide.imageSrc;
+
+
+  //ProgressBar Infos setzen
+  document.getElementById("progressBarLayer").style.backgroundColor = slide.progressBarColorBackground;
+  document.getElementById("progressBar").style.backgroundColor = slide.progressBarColor;
+
+  //Title Color setzen
+  document.getElementById("pechaKuchaTitle").style.color = slide.titleColor;
+
+  //Text Color setzen
+  document.getElementById("pechaKuchaText").style.color = slide.textColor;
+
+  //Border Color setzen
+  document.getElementById("container").style.borderColor = slide.borderColor;
+}
+
+function setProgressBar(secondsCount) {
+  var percent = 0;
+  var correctLength = timePerSlide - 1;
+  percent = 100 * secondsCount / correctLength;
+  var percentString = "";
+  percentString = percent + "%";
+  document.getElementById("progressBar").style.width = percentString;
+}
+
+function add1SecondFunction() {
+  // do whatever you like here
+  setProgressBar(seconds);
+  seconds = seconds + 1;
+  //document.getElementById("secondsIndicator").innerHTML = seconds; //Display Timer Value
+  if (seconds > timePerSlide) {
+    console.log("next slide");
+    nextSlide();
+    return;
+  }
+  setTimeout(add1SecondFunction, 1000);
+}
